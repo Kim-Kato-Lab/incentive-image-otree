f76a7df474649fcdfcc07585e4cf6c7ad49c8cb7
Hiroki Kato <hkato.econ@gmail.com>
2023-08-01 00:32:49 +0900

    add rebate incentive

--- donation/__init__.py
+++ donation/__init__.py
@@ -11,21 +11,36 @@ into account.
 class C(BaseConstants):
     NAME_IN_URL = 'donation'
     PLAYERS_PER_GROUP = 2
-    NUM_ROUNDS = 4
+    NUM_ROUNDS = 3
     HIGH_ROLE = 'High earner'
     LOW_ROLE = 'Low earner'
+    REBATE = [
+        [0.5, 0.15],
+        [0.5, 0.25],
+        [0.5, 0.5]#,
+        # [0.25, 0.5],
+        # [0.15, 0.5],
+        # [0.25, 0.1],
+        # [0.1, 0.25],
+        # [0.15, 0.15]
+    ]
 
 class Subsession(BaseSubsession):
     pass
 
 
 class Group(BaseGroup):
-    pass
+    list_row = models.IntegerField(initial=0)
+    high_income_rebate = models.FloatField(initial=0)
+    low_income_rebate = models.FloatField(initial=0)
 
 
 class Player(BasePlayer):
-    endowment = models.CurrencyField(initial=cu(0))
-    donate = models.CurrencyField(min=0)
+    endowment = models.IntegerField(initial=0)
+    donate = models.IntegerField(min=0)
+    rebate = models.FloatField(initial=0)
+    payoff_wo_rebate = models.IntegerField(initial=0)
+    payoff_w_rebate = models.IntegerField(initial=0)
 
 # FUNCTIONS
 def creating_session(subsession: Subsession):
@@ -39,9 +54,27 @@ def creating_session(subsession: Subsession):
     subsession.set_group_matrix(new_mat)
 
 
-def set_endowment(group: Group):
+def set_inc_rebate(group: Group):
+    # rebate incentive
+    rebate_list = list(range(len(C.REBATE)))
+    now_round = group.subsession.round_number
+    if now_round == 1:
+        pickup = random.sample(rebate_list, 1)[0]
+    else:
+        past_round = list(range(1, now_round))
+        done = [group.in_round(n).list_row for n in past_round]
+        for i in done:
+            rebate_list.pop(i)
+        pickup = random.sample(rebate_list, 1)[0]
+    group.list_row = pickup
+    group.high_income_rebate = C.REBATE[pickup][0]
+    group.low_income_rebate = C.REBATE[pickup][1]
+
+    # income
     high = group.get_player_by_role(C.HIGH_ROLE)
     low = group.get_player_by_role(C.LOW_ROLE)
+    high.rebate = group.high_income_rebate
+    low.rebate = group.low_income_rebate
     high.endowment = 75
     low.endowment = 25
 
@@ -51,12 +84,13 @@ def donate_max(player: Player):
 def set_payoffs(group: Group):
     players = group.get_players()
     for p in players:
-        p.payoff = p.endowment - p.donate
+        p.payoff_wo_rebate = p.endowment - p.donate
+        p.payoff_w_rebate = p.payoff_wo_rebate + int(round(p.rebate * p.donate, 0))
 
 
 # PAGES
 class WaitStart(WaitPage):
-    after_all_players_arrive = set_endowment
+    after_all_players_arrive = set_inc_rebate
 
 class Introduction(Page):
     @staticmethod
@@ -66,7 +100,11 @@ class Introduction(Page):
             partner = group.get_player_by_role(C.LOW_ROLE)
         else:
             partner = group.get_player_by_role(C.HIGH_ROLE)
-        return dict(partner_coin = partner.endowment) 
+        return dict(
+            your_rebate = int(player.rebate * 100),
+            partner_coin = partner.endowment,
+            partner_rebate = int(partner.rebate * 100)
+        ) 
 
 
 class Donate(Page):
