f3ccd334e7399c726721c2ac5b3eb114ddebde7d
Hiroki Kato <hkato.econ@gmail.com>
2023-07-31 18:16:24 +0900

    add recoding donation behavior

--- donation/__init__.py
+++ donation/__init__.py
@@ -11,46 +11,38 @@ into account.
 class C(BaseConstants):
     NAME_IN_URL = 'donation'
     PLAYERS_PER_GROUP = 2
-    NUM_ROUNDS = 9
-
+    NUM_ROUNDS = 2
+    ENDOWMENT = cu(70)
 
 class Subsession(BaseSubsession):
     pass
 
 
 class Group(BaseGroup):
-    kept = models.CurrencyField(
-        doc="""Amount dictator decided to keep for himself""",
-        min=0,
-        max=C.ENDOWMENT,
-        label="I will keep",
-    )
+    pass
 
 
 class Player(BasePlayer):
-    pass
+    donate = models.CurrencyField(min=0, max=C.ENDOWMENT)
 
 
 # FUNCTIONS
 def set_payoffs(group: Group):
-    p1 = group.get_player_by_id(1)
-    p2 = group.get_player_by_id(2)
-    p1.payoff = group.kept
-    p2.payoff = C.ENDOWMENT - group.kept
+    players = group.get_players()
+    for p in players:
+        p.payoff = C.ENDOWMENT - p.donate
 
 
 # PAGES
 class Introduction(Page):
-    pass
-
-
-class Offer(Page):
-    form_model = 'group'
-    form_fields = ['kept']
-
     @staticmethod
     def is_displayed(player: Player):
-        return player.id_in_group == 1
+        return player.round_number == 1
+
+
+class Donate(Page):
+    form_model = 'player'
+    form_fields = ['donate']
 
 
 class ResultsWaitPage(WaitPage):
@@ -58,11 +50,12 @@ class ResultsWaitPage(WaitPage):
 
 
 class Results(Page):
-    @staticmethod
-    def vars_for_template(player: Player):
-        group = player.group
-
-        return dict(offer=C.ENDOWMENT - group.kept)
+    pass
 
 
-page_sequence = [Introduction, Offer, ResultsWaitPage, Results]
+page_sequence = [
+    Introduction,
+    Donate,
+    ResultsWaitPage,
+    Results
+]
