33449634438ed5bf8a438efaf1feade8c6a4d44a
Hiroki Kato <hkato.econ@gmail.com>
2023-09-22 21:40:52 +0900

    add __init__ of quiz app

---
+++ quiz/__init__.py
@@ -0,0 +1,22 @@
+from otree.api import *
+
+
+class C(BaseConstants):
+    NAME_IN_URL = 'quiz'
+    PLAYERS_PER_GROUP = None
+    NUM_ROUNDS = 10
+
+class Subsession(BaseSubsession):
+    pass
+
+class Group(BaseGroup):
+    pass
+
+
+class Player(BasePlayer):
+    pass
+
+
+# FUNCTIONS
+
+# PAGES
